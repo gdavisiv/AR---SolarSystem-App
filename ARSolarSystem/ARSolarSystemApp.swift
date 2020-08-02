b791559d8c6fa2863879e47892d8d218db18cbf0
George Davis IV <GDavisIV@gmail.com>
2020-08-02 16:16:43 -0400

    Initial Commit

---
+++ ARSolarSystem/ARSolarSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARSolarSystemApp.swift
+//  ARSolarSystem
+//
+//  Created by George Davis IV on 8/2/20.
+//
+
+import SwiftUI
+
+@main
+struct ARSolarSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARSolarSystem/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ARSolarSystem
+//
+//  Created by George Davis IV on 8/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
